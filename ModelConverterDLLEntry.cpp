ccc771ceacaba6e15a34269c9079c0d158d50706
unknown <dimaskupejko@gmail.com>
2023-08-05 18:26:10 +0300

    v.001; global refactoring of work with the .obj file type (1)

---
+++ ModelConverterDLLEntry.cpp
@@ -0,0 +1,33 @@
+#define MODEL_CONVERTER_DLL_ENTRY_EXPORTS
+
+#include "ModelConverterDLLEntry.h"
+#include "ModelConverterClass.h"
+#include <iostream>
+
+
+void ModelConverter::ImportModelFromFile(
+	const char* inputFilename,      // full path to the model's input data file 
+	const char* outputFilename)     // full path to the model's output data file
+{
+	assert((inputFilename != nullptr) && (inputFilename[0] != '\0'));
+	assert((outputFilename != nullptr) && (outputFilename[0] != '\0'));
+
+
+	// check if we already have an OUTPUT data file for the model of such type
+	std::ifstream fin(outputFilename, std::ios::in | std::ios::binary);
+	bool executeModelConvertation = executeModelConvertation = fin.fail();
+
+	// if we need to convert external file model data into the internal model format
+	if (true)
+	{
+		std::unique_ptr<ModelConverterClass> pModelConverter = std::make_unique<ModelConverterClass>();
+
+		bool result = pModelConverter->ConvertFromObj(inputFilename, outputFilename);
+		if (!result)
+		{
+			std::cout << "can't convert .obj into the internal model format" << std::endl;
+			return;
+		}
+	}
+
+}
---
+++ ModelConverterDLLEntry.h
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace ModelConverter
+{
+	// when the MODEL_CONVERTER_DLL_EXPORTS macro is defined, the MODEL_CONVERTER_API
+	// macro sets the __declspec(dllexport) modifier on the function declarations. This
+	// modifier tells the compiler and linker to export a function or variable from the DLL
+	// for use by other applications. Then MODEL_CONVERTER_DLL_EXPORTS is undefined, for 
+	// example, wneh the header file is included by a client application,
+	// MODEL_CONVERTER_API applies the __declspec(dllimport) modifier to the declarations.
+	// This modifier optimizes the import of the function or variable in an application.
+	#ifdef MODEL_CONVERTER_DLL_ENTRY_EXPORTS
+		#define MODEL_CONVERTER_API __declspec(dllexport)
+	#else
+		#define MODEL_CONVERTER_API __declspec(dllimport)
+	#endif
+
+
+	// DEFINE THE DLL's INTERFACE
+
+	//#ifdef __cplusplus    // if used by C++ code,
+	//	extern "C" {      // we need to export the C interface
+	//#endif
+
+
+	extern "C" MODEL_CONVERTER_API void ImportModelFromFile(
+		const char* inputFilename,      // full path to the model's input data file 
+		const char* outputFilename);    // full path to the model's output data file
+
+	//#ifdef __cplusplus    // if used by C++ code,
+	//	}                 // the end of "extern C" declaration
+	//#endif
+}
+
+
+
+
